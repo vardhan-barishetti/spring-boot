ebd9187c321ac267d03030a2ae8f5470d6ba5321
vardhan barishetti <67987365+vardhan-barishetti@users.noreply.github.com>
2024-11-02 16:24:46 -0400

    Initial commit

---
+++ src/main/java/com/example/spring_boot_core/SpringBootCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCoreApplication.class, args);
+	}
+
+}
